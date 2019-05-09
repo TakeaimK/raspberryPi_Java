bdb3d26e03cf435a0701d6b418cba53480454315
TakeaimK <csj7686@naver.com>
2019-05-09 10:23:02 +0900

    burger_test

--- raspberryPi_GUI/src/raspberryPi_GUI/foodGUI.java
+++ raspberryPi_GUI/src/raspberryPi_GUI/foodGUI.java
@@ -1,232 +1,246 @@
 package raspberryPi_GUI;
-
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JTextField;
+ 
 import java.awt.BorderLayout;
-import javax.swing.JButton;
-import javax.swing.BoxLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.Button;
 import java.awt.Color;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 import java.awt.Font;
-import javax.swing.JSplitPane;
-import javax.swing.JToolBar;
-import java.awt.SystemColor;
-import javax.swing.JTable;
-
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+ 
 public class foodGUI {
-
-	private JFrame frame;
-	private JTable table;
-	
-	//need variable!
-	int but1_cost=2000;
-	int but2_cost=3000;
-	int but3_cost=5000;
-	int but4_cost=7000;
-	int but5_cost=10000;
-	int but6_cost=12000;
-	int total_cost=0;	//RFID load data input
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					foodGUI window = new foodGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public foodGUI() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	 public void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setForeground(new Color(255, 255, 255));
-		frame.getContentPane().setBackground(new Color(255, 250, 205));
-		frame.setBounds(100, 100, 764, 487);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		
-		JLabel costLabel = new JLabel("13,000");
-		costLabel.setForeground(new Color(0, 0, 0));
-		costLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		costLabel.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.PLAIN, 40));
-		costLabel.setBounds(306, 373, 152, 42);
-		costLabel.setText(total_cost+"");
-		frame.getContentPane().add(costLabel);
-		
-		
-		JButton button1 = new JButton("Button1");
-		button1.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 20));
-		button1.setBackground(new Color(255, 255, 224));
-		button1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				total_cost+=but1_cost;
-				costLabel.setText(total_cost+"");
-			}
-		});
-		
-		button1.setBounds(40, 70, 129, 132);
-		frame.getContentPane().add(button1);
-		
-		JButton button2 = new JButton("Button2");
-		button2.setBackground(new Color(255, 255, 224));
-		button2.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 20));
-		button2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				total_cost+=but2_cost;
-				costLabel.setText(total_cost+"");
-			}
-		});
-		button2.setBounds(181, 70, 129, 132);
-		frame.getContentPane().add(button2);
-		
-		JButton button3 = new JButton("Button3");
-		button3.setBackground(new Color(255, 255, 224));
-		button3.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 20));
-		button3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				total_cost+=but3_cost;
-				costLabel.setText(total_cost+"");
-			}
-		});
-		button3.setBounds(322, 70, 129, 132);
-		frame.getContentPane().add(button3);
-		
-		JButton button4 = new JButton("Button4");
-		button4.setBackground(new Color(255, 255, 224));
-		button4.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 20));
-		button4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				total_cost+=but4_cost;
-				costLabel.setText(total_cost+"");
-			}
-		});
-		button4.setBounds(40, 212, 129, 132);
-		frame.getContentPane().add(button4);
-		
-		JButton button5 = new JButton("Button5");
-		button5.setBackground(new Color(255, 255, 224));
-		button5.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 20));
-		button5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				total_cost+=but5_cost;
-				costLabel.setText(total_cost+"");
-			}
-		});
-		button5.setBounds(181, 212, 129, 132);
-		frame.getContentPane().add(button5);
-		
-		JButton button6 = new JButton("Button6");
-		button6.setBackground(new Color(255, 255, 224));
-		button6.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 20));
-		button6.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				total_cost+=but6_cost;
-				costLabel.setText(total_cost+"");
-			}
-		});
-		button6.setBounds(322, 212, 129, 132);
-		frame.getContentPane().add(button6);
-		
-		JLabel lblWelcomeToThe = new JLabel("Welcome to the Susi Nara!");
-		lblWelcomeToThe.setForeground(new Color(0, 0, 0));
-		lblWelcomeToThe.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.PLAIN, 47));
-		lblWelcomeToThe.setHorizontalAlignment(SwingConstants.CENTER);
-		lblWelcomeToThe.setBounds(12, 11, 508, 50);
-		frame.getContentPane().add(lblWelcomeToThe);
-		
-		JLabel payment = new JLabel("Advertisement");
-		payment.setBackground(new Color(0, 0, 0));
-		payment.setForeground(new Color(255, 0, 0));
-		payment.setFont(new Font("a¿¸¿⁄Ω√∞Ë", Font.PLAIN, 17));
-		payment.setHorizontalAlignment(SwingConstants.CENTER);
-		payment.setBounds(513, 337, 189, 103);
-		frame.getContentPane().add(payment);
-		
-		JLabel lblUser = new JLabel("User 1 : ");
-		lblUser.setForeground(new Color(0, 0, 0));
-		lblUser.setHorizontalAlignment(SwingConstants.CENTER);
-		lblUser.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.PLAIN, 40));
-		lblUser.setBounds(222, 373, 120, 42);
-		frame.getContentPane().add(lblUser);
-		
-		JLabel label_1 = new JLabel("\\");
-		label_1.setForeground(new Color(0, 0, 0));
-		label_1.setHorizontalAlignment(SwingConstants.CENTER);
-		label_1.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.PLAIN, 40));
-		label_1.setBounds(445, 373, 37, 42);
-		frame.getContentPane().add(label_1);
-		
-		JButton Payment = new JButton("¡÷πÆ«œ±‚");
-		Payment.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 20));
-		Payment.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		Payment.setBounds(513, 285, 189, 42);
-		frame.getContentPane().add(Payment);
-		
-		JButton button7 = new JButton("<-");
-		button7.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 24));
-		button7.setForeground(new Color(100, 149, 237));
-		button7.setBackground(new Color(100, 149, 237));
-		button7.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		button7.setBounds(513, 29, 91, 30);
-		frame.getContentPane().add(button7);
-		
-		JButton button8 = new JButton("->");
-		button8.setForeground(new Color(255, 69, 0));
-		button8.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 23));
-		button8.setBackground(new Color(255, 69, 0));
-		button8.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		button8.setBounds(609, 29, 91, 30);
-		frame.getContentPane().add(button8);
-		
-		JButton button9 = new JButton("¡÷πÆªÛ≈¬");
-		button9.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 12));
-		button9.setBounds(11, 358, 71, 74);
-		frame.getContentPane().add(button9);
-		
-		JButton button10 = new JButton("∞·¡¶");
-		button10.setFont(new Font("≥™¥Æº’±€ææ ∆Ê", Font.BOLD, 12));
-		button10.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		button10.setBounds(83, 358, 74, 74);
-		frame.getContentPane().add(button10);
-		
-		table = new JTable();
-		table.setColumnSelectionAllowed(true);
-		table.setCellSelectionEnabled(true);
-		table.setBounds(513, 70, 189, 208);
-		frame.getContentPane().add(table);
-	}
+    int count = 0;
+    String show = "";
+ 
+    public foodGUI() {
+ 
+        // ÎîîÏûêÏù∏Îã®
+        // ÌîÑÎ†àÏûÑ ÏÑ§Ï†ïÎã®
+        JFrame frame = new JFrame("ÌñÑÎ≤ÑÍ±∞ ÏûêÎèô ÌåêÎß§Í∏∞");
+        frame.setBounds(0, 0, 625, 1000);
+        frame.setBackground(Color.black);
+ 
+        // Ìè∞Ìä∏
+        Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 18);
+        Font font1 = new Font(Font.MONOSPACED, Font.BOLD, 22);
+ 
+        // Î∂ÅÏ™Ω
+        Panel pNorth = new Panel();
+        pNorth.setBackground(new Color(255, 255, 215));
+        pNorth.setLayout(null);
+        pNorth.setSize(0, 500);
+        pNorth.setFont(font);
+ 
+        // Î∞∞Ïó¥ ÏÑ§Ï†ï Î∂ÄÎ∂Ñ
+        String menu[] = { "ÎπÖÎß•Î≤ÑÍ±∞", "Ïã∏Ïù¥Î≤ÑÍ±∞", "ÎçîÎ∏îÎ≤ÑÍ±∞", "ÎßòÎßàÎ≤ÑÍ±∞", "ÍπÄÏπòÎ≤ÑÍ±∞", "ÏÉàÏö∞Î≤ÑÍ±∞", "ÎùºÎ©¥Î≤ÑÍ±∞", "ÏπòÌÇ®Î≤ÑÍ±∞" };
+        int price[] = { 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500 };
+        JButton bt[] = new JButton[menu.length];
+        TextField suja[] = new TextField[menu.length];
+        Button minus[] = new Button[menu.length];
+        Button plus[] = new Button[menu.length];
+        JButton ok[] = new JButton[menu.length];
+        Label l[] = new Label[menu.length];
+        ImageIcon icon[] = new ImageIcon[menu.length];
+ 
+        // Î≤ÑÌäº ÏÑ§Ï†ï Î∂ÄÎ∂Ñ
+        for (int i = 0; i < menu.length; i++) {
+ 
+            // ÌñÑÎ≤ÑÍ±∞ Î≤ÑÌäº
+            bt[i] = new JButton(menu[i]);
+            if (i < 4) {
+                bt[i].setBounds(25 + i * 150, 50, 100, 100);
+            } else {
+                bt[i].setBounds(25 + (i - 4) * 150, 300, 100, 100);
+            }
+            icon[i] = new ImageIcon(i + ".png");
+            bt[i].setIcon(icon[i]);
+ 
+            // Ïà´Ïûê txt Î≤ÑÌäº
+            suja[i] = new TextField("0");
+            suja[i].setBackground(Color.white);
+            suja[i].setEditable(false);
+            suja[i].setBounds(bt[i].getX() + 30, bt[i].getY() + 130, 40, 20);
+ 
+            // "-" Î≤ÑÌäº
+            minus[i] = new Button("-");
+            minus[i].setBounds(bt[i].getX(), suja[i].getY(), 20, 20);
+            minus[i].setEnabled(false);
+ 
+            // "+" Î≤ÑÌäº
+            plus[i] = new Button("+");
+            plus[i].setBounds(bt[i].getX() + (100 - 20), suja[i].getY(), 20, 20);
+            plus[i].setEnabled(false);
+ 
+            // Í∞ÄÍ≤©
+            l[i] = new Label(price[i] + "Ïõê");
+            l[i].setBounds(bt[i].getX() + 20, suja[i].getY() - 25, 100, 20);
+ 
+            // ÌôïÏù∏ Î≤ÑÌäº
+            ok[i] = new JButton("ÌôïÏù∏");
+            ok[i].setBounds(bt[i].getX(), suja[i].getY() + 30, 100, 20);
+            ok[i].setEnabled(false);
+ 
+            pNorth.add(bt[i]);
+            pNorth.add(suja[i]);
+            pNorth.add(minus[i]);
+            pNorth.add(plus[i]);
+            pNorth.add(l[i]);
+            pNorth.add(ok[i]);
+        }
+ 
+        // Ï§ëÏïô
+        TextArea ta = new TextArea("", 0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
+        ta.setText("   ÏÉÅÌíàÎ™Ö        Îã®Í∞Ä        ÏàòÎüâ        Ìï©Í≥Ñ\n\n");
+        ta.setBackground(Color.white);
+        ta.setEditable(false);
+        ta.setFont(font1);
+ 
+        // ÎÇ®Ï™Ω
+        Panel pSouth = new Panel();
+        pSouth.setFont(font);
+        pSouth.setBackground(new Color(255, 255, 215));
+ 
+        Button bt1 = new Button("Ï£ºÎ¨∏");
+        Button bt2 = new Button("Ï¥àÍ∏∞Ìôî");
+        Button bt3 = new Button("Îã´Í∏∞");
+        pSouth.add(bt1);
+        pSouth.add(bt2);
+        pSouth.add(bt3);
+ 
+        // Ï£ºÎ¨∏Î≤ÑÌäº
+        bt1.addActionListener(new ActionListener() {
+ 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, ta.getText() + " Ï£ºÎ¨∏ÎêòÏóàÏäµÎãàÎã§. \nÏù¥Ïö©Ìï¥Ï£ºÏÖîÏÑú Í∞êÏÇ¨Ìï©ÎãàÎã§.");
+                for (int i = 0; i < menu.length; i++) {
+                    bt[i].setEnabled(true);
+                    minus[i].setEnabled(false);
+                    plus[i].setEnabled(false);
+                    suja[i].setText("0");
+                    ta.setText("   ÏÉÅÌíàÎ™Ö        Îã®Í∞Ä        ÏàòÎüâ        Ìï©Í≥Ñ\n\n");
+ 
+                }
+            }
+        });
+ 
+        // Ï¥àÍ∏∞Ìôî Î≤ÑÌäº
+        bt2.addActionListener(new ActionListener() {
+ 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < menu.length; i++) {
+                    bt[i].setEnabled(true);
+                    minus[i].setEnabled(false);
+                    plus[i].setEnabled(false);
+                    suja[i].setText("0");
+                    ta.setText("   ÏÉÅÌíàÎ™Ö        Îã®Í∞Ä        ÏàòÎüâ        Ìï©Í≥Ñ\n\n");
+ 
+                }
+            }
+        });
+ 
+ 
+        //bt3 Îã´Í∏∞Î≤ÑÌäº
+        
+        bt3.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+ 
+ 
+        // Ïª¥Ìè¨ÎÑåÌä∏
+        frame.add(pNorth, BorderLayout.NORTH);
+        frame.add(ta, BorderLayout.CENTER);
+        frame.add(pSouth, BorderLayout.SOUTH);
+        frame.setVisible(true);
+ 
+        // Ïù¥Î≤§Ìä∏Îã®
+        for (int i = 0; i < menu.length; i++) {
+            int j = i;
+ 
+            // ÌñÑÎ≤ÑÍ∑∏ Î≤ÑÌäº Ïù¥Î≤§Ìä∏
+            bt[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    minus[j].setEnabled(true);
+                    plus[j].setEnabled(true);
+                    bt[j].setEnabled(false);
+                    ok[j].setEnabled(true);
+ 
+                    count = 0;
+                }
+            });
+ 
+            // "-" Î≤ÑÌäº Ïù¥Î≤§Ìä∏
+            minus[i].addActionListener(new ActionListener() {
+ 
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (count > 0) {
+                        count = count - 1;
+                        suja[j].setText(count + "");
+                        ok[j].setEnabled(true);
+                    } else {
+                        minus[j].setEnabled(false);
+                    }
+                }
+            });
+            
+            // "+" Î≤ÑÌäº Ïù¥Î≤§Ìä∏
+            plus[i].addActionListener(new ActionListener() {
+ 
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    count = count + 1;
+                    suja[j].setText(count + "");
+                    ok[j].setEnabled(true);
+                    if (count > 0) {
+                        minus[j].setEnabled(true);
+                    }
+                }
+            });
+            
+            //ÌôïÏù∏ Î≤ÑÌäº Ïù¥Î≤§Ìä∏
+            ok[i].addActionListener(new ActionListener() {
+ 
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    show = bt[j].getActionCommand();
+                    ta.append("   " + show + "       " + price[j] + "        " + count + "         " + price[j] * count
+                            + "Ïõê" + "\n");
+                    ok[j].setEnabled(false);
+                }
+            });
+ 
+        }
+ 
+        // ÎÅÑÍ∏∞
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+ 
+    // Î©îÏù∏
+    public static void main(String[] args) {
+        new foodGUI();
+    }
+ 
 }
+
+
